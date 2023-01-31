120af8c2d9e1be2a0dee2fbba978ac56d7b7989c
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-01-31 18:21:24 +0530

    Added dispatch method for categories page

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatusAction } from '../redux/categories/categories';
 
 export default function Category() {
+  const dispatch = useDispatch();
+  const clickHandler = () => {
+    dispatch(checkStatusAction());
+  };
+
   return (
     <div>
-      <button type="button">Check Status</button>
+      <button type="button" onClick={clickHandler}>Check Status</button>
     </div>
   );
 }
